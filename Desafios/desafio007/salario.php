94139157ff4a7750d854d5fae1e3c64e2931b36f
rogervalentim <valentim.roger33@gmail.com>
2023-04-27 21:33:38 -0300

    desafio 008

--- Desafios/desafio007/salario.php
+++ Desafios/desafio007/salario.php
@@ -9,14 +9,14 @@
 </head>
 <body>
     <?php 
-    $salário_mínimo = 1380;
-    $salário = $_GET["salario"] ?? 0;
+    $salário_mínimo = 1380.60;
+    $salário = $_GET["salario"] ?? 1380.60;
     $padrão = numfmt_create('pt_BR', NumberFormatter::CURRENCY);
 
      ?>
     <main>
-        <form action="<?= $_SERVER['PHP_SELF'] ?>">
         <h1>Informe seu salário</h1>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>">
         <label for="salario">Salário (R$)</label>
         <input type="number" name="salario" id="salario" step="0.01" value="<?= $salário ?>">
         <p>Considerando o salário mínimo de <Strong><?= numfmt_format_currency($padrão, $salário_mínimo, 'BRL') ?></strong></p>
@@ -25,12 +25,12 @@
     </main>
     <section>
         <?php 
-        $quociente = $salário / $salário_mínimo;
+        $quociente = intdiv($salário, $salário_mínimo);
         $resto = $salário % $salário_mínimo;
         ?>
 
         <h2>Resultado Final</h2>
-        <p>Quem recebe um salário de <?= numfmt_format_currency($padrão, $salário, 'BRL') ?> ganha <strong><?= floor($quociente) ?> salários mínimos</strong> + <br> <?= numfmt_format_currency($padrão, $resto, 'BRL') ?> </p>
+        <p>Quem recebe um salário de <?= numfmt_format_currency($padrão, $salário, 'BRL') ?> ganha <strong><?= $quociente ?> salários mínimos</strong> + <br> <?= numfmt_format_currency($padrão, $resto, 'BRL') ?> </p>
     </section>
 </body>
 </html>
---
+++ Desafios/desafio008/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio das raizes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $numero = $_GET['num'] ?? 0;
+    $raiz_quadrada = sqrt($numero);
+    $raiz_cúbica = $numero ** (1/3);
+    ?>
+
+    <main>
+        <h1>Informe um número</h1>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>">
+            <label for="num">Número</label>
+            <input type="text" name="num" id="num" value="<?= $numero ?>">
+            <input type="submit" value="Calcular Raízes">
+        </form>
+    </main>
+
+    <section>
+        <h2>Resultado final</h2>
+        <p>Analisando o <strong>número <?= $numero ?></strong>, temos:</p>
+        <ul>
+            <li>A sua raiz quadrada é <strong><?= number_format($raiz_quadrada, 3, ",", ".") ?></strong></li>
+            <li>A sua raiz cúbica é <strong><?= number_format($raiz_cúbica, 3, ",", "." ) ?></strong></li>
+        </ul>
+    </section>
+</body>
+</html>
